296c1c6cc173af37654a84c4824dc48e665e3030
Amar <mohamed.zewin27@gmail.com>
2023-09-09 00:05:12 +0300

    create note Screen

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+static const String routeName='NotesView';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
