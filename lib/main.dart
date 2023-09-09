64b93adfb7d5a8e932ddc899cc4367eb5da31191
Amar <mohamed.zewin27@gmail.com>
2023-09-09 05:28:17 +0300

    MultiBlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'package:note/constants/constantForApp.dart';
+import 'package:note/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note/model/noteModel.dart';
 import 'package:note/views/editNoteView.dart';
 import 'package:note/views/notes_view.dart';
@@ -19,14 +21,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: fontsForNote),
-      initialRoute: NotesView.routeName,
-      routes: {
-        NotesView.routeName: (context) => NotesView(),
-        EditNoteView.routeName: (context) => EditNoteView(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: fontsForNote),
+        initialRoute: NotesView.routeName,
+        routes: {
+          NotesView.routeName: (context) => NotesView(),
+          EditNoteView.routeName: (context) => EditNoteView(),
+        },
+      ),
     );
   }
 }
